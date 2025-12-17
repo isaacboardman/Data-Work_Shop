6f93a1d0ca3bd53f564f43bf06667f94290784a8
Isaac Boardman <129878980+isaacboardman@users.noreply.github.com>
2025-12-16 20:38:41 -0700

    Delete run.sh

--- run.sh
+++
@@ -1,1 +0,0 @@
-ogr2ogr earthquake.parquet "/vsicurl/https://services9.arcgis.com/RHVPKKiFTONKtxq3/arcgis/rest/services/USGS_Seismic_Data_v1/FeatureServer/0/query?f=geojson&where=1%3D1&returnGeometry=true&spatialRel=esriSpatialRelIntersects&outFields=*&orderByFields=mag%20DESC&outSR=3857&resultOffset=0&resultRecordCount=10000"
